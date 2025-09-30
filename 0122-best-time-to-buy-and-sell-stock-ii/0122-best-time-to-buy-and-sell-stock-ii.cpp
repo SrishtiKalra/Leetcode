013bafb8cac5a67b83ff1dc684759d11c3c145f3
Srishti Kalra <47745643+SrishtiKalra@users.noreply.github.com>
2025-09-30 13:56:08 -0400

    Time: 9 ms (7.98%), Space: 22.2 MB (9.77%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int func(vector<int>& prices, int ind, int buy, vector<vector<int>>& dp){
+        if(ind>=prices.size()){
+            return 0;
+        }
+        if(dp[ind][buy]!=-1){
+            return dp[ind][buy];
+        }
+        int profit = 0;
+
+
+        if(buy){
+
+            profit = max(-prices[ind]+func(prices, ind+1,0,dp), func(prices, ind+1,1,dp));
+
+        }
+        else{
+            profit = max(prices[ind]+func(prices, ind+1,1,dp), func(prices, ind+1,0,dp)); 
+        }
+
+        return dp[ind][buy] = profit; 
+
+    }
+
+
+    int maxProfit(vector<int>& prices) {
+
+        vector<vector<int>> dp(prices.size(), vector<int>(2,-1));
+        //dp[0][0]=1;
+        return func(prices, 0, 1, dp);
+
+        
+    }
+};
